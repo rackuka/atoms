fa41d7de63856f86576f0646ac4c49bc82f865f3
Rackuka <rackuka@gmail.com>
2021-05-13 16:43:27 +0300

    test code in App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SliderInput } from './rne/SliderInput';
+import { Input, Slider } from 'react-native-elements';
 
 export default function App() {
+  const [sliderInputVal, setSliderInputVal] = useState(0)
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Slider 
+            value={sliderInputVal} 
+            step={1} 
+            minimumValue={1} 
+            maximumValue={40} 
+            onValueChange={(val) => {setSliderInputVal(val)}}
+            style={{
+              borderColor: 'black'
+            }}
+        />
+        <SliderInput 
+            value={sliderInputVal} 
+            step={1} 
+            minimumValue={1} 
+            maximumValue={40} 
+            onValueChange={(val) => {setSliderInputVal(val)}} 
+            style={{
+              borderColor: 'green'
+            }}
+        />
+        <Slider 
+            value={sliderInputVal} 
+            step={1} 
+            minimumValue={1} 
+            maximumValue={40} 
+            onValueChange={(val) => {setSliderInputVal(val)}}
+            style={{
+              borderColor: 'black'
+            }}
+        />
+        <Input keyboardType='numeric' value={`${sliderInputVal}`} 
+              onChangeText={
+                (val:string) => {
+                    let iVal = Number.parseInt(val);
+                    let vVal = Number.isNaN(iVal) ? 0 : iVal;
+                    setSliderInputVal(vVal);
+                  }
+        }/>
+      </View>
+    </SafeAreaView>
   );
 }
 
