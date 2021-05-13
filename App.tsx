32adab2fe6f3ace1cc2d5d879674947b29200af6
Rackuka <rackuka@gmail.com>
2021-05-13 22:27:07 +0300

    App - cleanup lil bit

--- App.tsx
+++ App.tsx
@@ -2,55 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { SliderInput } from './rne/SliderInput';
-import { Input, Slider } from 'react-native-elements';
+import { ButtonGroup, Input, Slider } from 'react-native-elements';
 
 export default function App() {
-  const [sliderInputVal, setSliderInputVal] = useState(0)
-
+  const [sliderInputVal, setSliderInputVal] = useState(1)
 
   return (
     <SafeAreaView>
-      <View>
+      <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style="auto" />
-        <Slider 
-            value={sliderInputVal} 
-            step={1} 
-            minimumValue={1} 
-            maximumValue={40} 
-            onValueChange={(val) => {setSliderInputVal(val)}}
-            style={{
-              borderColor: 'black'
-            }}
-        />
         <SliderInput 
             value={sliderInputVal} 
-            step={1} 
+            step={5} 
             minimumValue={1} 
             maximumValue={40} 
             onValueChange={(val) => {setSliderInputVal(val)}} 
             style={{
-              borderColor: 'green'
+              borderColor: 'green',
+              paddingLeft: '20'
             }}
         />
-        <Slider 
-            value={sliderInputVal} 
-            step={1} 
-            minimumValue={1} 
-            maximumValue={40} 
-            onValueChange={(val) => {setSliderInputVal(val)}}
-            style={{
-              borderColor: 'black'
-            }}
-        />
-        <Input keyboardType='numeric' value={`${sliderInputVal}`} 
-              onChangeText={
-                (val:string) => {
-                    let iVal = Number.parseInt(val);
-                    let vVal = Number.isNaN(iVal) ? 0 : iVal;
-                    setSliderInputVal(vVal);
-                  }
-        }/>
       </View>
     </SafeAreaView>
   );
@@ -58,9 +30,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
