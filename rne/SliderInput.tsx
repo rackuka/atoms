64d0ac3c9d696bf37ab630996eae6c6162cec4cd
Rackuka <rackuka@gmail.com>
2021-05-13 22:26:48 +0300

    SliderInput - slider props propagated to props

--- rne/SliderInput.tsx
+++ rne/SliderInput.tsx
@@ -1,43 +1,61 @@
 import React, { useEffect, useState } from 'react'
+import { View, StyleSheet } from 'react-native';
 import { Input, Slider, SliderProps, Text } from 'react-native-elements'
 
 interface SliderInputProps {
 }
 
 export const SliderInput: React.FC<SliderInputProps & SliderProps> = (props) => {
-  const minValue: number = props.minimumValue ?? Number.MIN_VALUE;
-  const maxValue: number = props.maximumValue ?? Number.MAX_VALUE;
+  const minValue: number = props.minimumValue ?? 0;
+  const maxValue: number = props.maximumValue ?? 1;
 
   function onValueChange(val: number) {
-    setValue(val)
+  setValue(val)
     if (props.onValueChange !== undefined) {
       props.onValueChange(val)
     }
   }
 
-  const [value, setValue] = useState<number>(props.value ?? 0)
+  const [value, setValue] = useState<number>(props.value === undefined ? minValue : props.value < minValue ? minValue : props.value > maxValue ? maxValue : props.value)
 
   useEffect(() => {
-    setValue(props.value ?? 0)
+    setValue(props.value ?? minValue)
   }, [props.value])
-  
+
   return (
-    <>
+    <View style={styles.container}>
       <Slider
         step={props.step}
+        style={styles.slider || props.style || props.containerStyle}
+        thumbStyle={props.thumbStyle}
         value={value}
         minimumValue={minValue}
         maximumValue={maxValue}
         onValueChange={(val) => {onValueChange(val)}}
       />
       <Input keyboardType='numeric' value={`${value}`} 
+             containerStyle={styles.input}
              onChangeText={
                (val:string) => {
                   let iVal = Number.parseInt(val);
-                  let vVal = Number.isNaN(iVal) ? 0 : iVal;
+                  let vVal = Number.isNaN(iVal) ? minValue : iVal < minValue ? minValue : iVal > maxValue ? maxValue : iVal;
                   onValueChange(vVal);
                 }
       }/>
-    </>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  slider: {
+    flex: 6,
+  },
+  input: {
+    flex: 2,
+    height: 40
+  },
+});
